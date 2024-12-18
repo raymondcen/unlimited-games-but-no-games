62d92812bd72416f8eb4be3e96566d275b644841
IvanW5X <ivanwong169@gmail.com>
2024-12-17 23:28:12 -0800

    More project setup

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,32 @@
  ****************************************************************************/
 
 
-#include <iostream>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QObject>
+
+#include <iostream>
 
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    // const QUrl url(QStringLiteral("qrc:/resources/qml/mainWindow.qml"));
+
+    // QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+    //                  &app, [url](QObject *obj, const QUrl &objUrl) {
+    //     if (!obj && url == objUrl) {
+    //         qDebug() << "Failed to load QML file";
+    //         QCoreApplication::exit(-1);
+    //     }
+    // }, Qt::QueuedConnection);
+
+    // engine.load(url);
+
+    // // Run app
+    // return app.exec();
 
     return 0;
 }
