1d8467524af8027a266fe4affa4b34eb8c875416
IvanW5X <ivanwong169@gmail.com>
2024-12-17 22:29:46 -0800

    Set up project

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/****************************************************************************
+ * File Name:
+ * Date:
+ * Description:
+ * Dependencies:
+ * Author(s): Raymond Cen,
+ *            Ivan Wong
+ ****************************************************************************/
+
+
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
