28fbe662910d9f3471ed76158c7445cc0b2fd593
raymondcen <raymondcen07@gmail.com>
2024-12-18 23:05:45 -0800

    updated border. added small, med, large grid options.

--- games/snake/snake.cpp
+++ games/snake/snake.cpp
@@ -15,21 +15,26 @@ int Snake::snakeGame(){
     
     const int screenWidth = 1920;
     const int screenHeight = 1080;
-
-    int border= screenWidth*.02;
-
-
+ 
     //resize window
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screenWidth, screenHeight, "Ekans");
-
     ToggleFullscreen();
 
-    //size of cells
-    int cellSize = 40;
+    //size of grid
+    int smallGrid=120;
+    int medGrid=60; //idk
+    int largeGrid=60;
+
+    int cellSize = smallGrid;
+
+    int border= cellSize;
+
+    
+
 
     //init snake object
-    Snake snake(screenWidth/2, screenHeight/2, 1);
+    //Snake snake(screenWidth/2, screenHeight/2, 1);
 
 
     SetTargetFPS(60);              
@@ -48,16 +53,15 @@ int Snake::snakeGame(){
             DrawRectangle(screenWidth-border, 0, screenWidth, screenHeight, BLUE); //right
             DrawRectangle(0, screenHeight-border, screenWidth, screenHeight, BLUE); //bottom
 
-
-
             //DrawGrid but 2D
-            for(int x = 1; x <= screenWidth; x += cellSize){
-                DrawLine(x, 0, x, screenHeight, RED);
-                for(int y=0; y <= screenHeight; y += cellSize){
-                    DrawLine(x, y, screenWidth, y, RED);
-                }
+            for (int x = border;x <= screenHeight- border;x += cellSize) {
+                DrawLine(border, x, screenWidth - border, x, RED);
             }
 
+            for (int y = border;y <= screenWidth- border;y += cellSize) {
+                DrawLine(y, border, y, screenHeight -border, RED);
+            }
+            
             //draw snake
             //DrawRectangle(1, 1, cellSize, cellSize, GREEN); 
 
