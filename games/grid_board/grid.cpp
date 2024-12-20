5df1bac99c12d0e7b27a9c1ce93dc98530f824d9
IvanW5X <ivanwong169@gmail.com>
2024-12-19 19:25:58 -0800

    Added Comments to grid Class

--- games/grid_board/grid.cpp
+++ games/grid_board/grid.cpp
@@ -12,6 +12,7 @@
 
 
 Grid::Grid(int rows, int cols, int cell_size) {
+    // Set grid size and initialize grid vector
     this->num_rows = rows;
     this->num_columns = cols;
     this->cell_size = cell_size;
@@ -23,11 +24,13 @@ Grid::~Grid() {}
 
 
 void Grid::initialize_grid() {
+    // Resize vector
     this->grid.resize(this->num_rows);
 
     for (int i = 0 ; i < this->num_rows; i++)
         this->grid[i].resize(num_columns);
 
+    // Initialize all entries to 0
     for (int i = 0; i < this->num_rows; i++) {
         for (int j = 0; j < this->num_columns; j++) {
             this->grid[i][j] = 0;
@@ -37,9 +40,11 @@ void Grid::initialize_grid() {
 
 
 void Grid::draw_grid() {
+    // Draw each square for the grid
     for (int i = 0; i < this->num_rows; i++) {
         for (int j = 0; j < this->num_columns; j++) {
-            DrawRectangle(j * this->cell_size + 1, i * this->cell_size + 1, this->cell_size - 1, this->cell_size - 1, BLACK);
+            DrawRectangle(j * this->cell_size + 1, i * this->cell_size + 1, 
+                          this->cell_size - 1, this->cell_size - 1, BLACK);
         }
     }
 }
