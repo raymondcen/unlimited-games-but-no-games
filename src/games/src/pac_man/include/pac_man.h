63f2f40782efcdcf7b290d6cd31c4a4037fa056c
raymondcen <raymondcen07@gmail.com>
2024-12-30 23:31:58 -0800

    added map

--- src/games/src/pac_man/include/pac_man.h
+++ src/games/src/pac_man/include/pac_man.h
@@ -21,6 +21,8 @@
 
 // Custom Libraries
 #include "game.h"
+#include "grid.h"
+
 
 
 class PacMan : public Game {
@@ -28,10 +30,18 @@ class PacMan : public Game {
 private:
     Vector2 position;
 
+    int map[22][19];
+    int num_columns;
+    int num_rows;
+    int cell_size;
+
 public:
     PacMan();
     ~PacMan();
 
     void run_game();
+    void draw_grid2(int, int);
+
+    void initiate_map();
 
 };
