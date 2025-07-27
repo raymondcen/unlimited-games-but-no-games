7e65eec35d3d69315c317adbfe8446b4ae75ef14
Raymond Cen <raymondcen07@gmail.com>
2025-07-26 20:25:54 -0700

    pacman wall collision

--- src/games/src/pac_man/src/pac_man.cpp
+++ src/games/src/pac_man/src/pac_man.cpp
@@ -26,43 +26,104 @@ PacMan::~PacMan() {}
 
 void PacMan::draw_pacman(){
     //DrawTextureEx(Texture2D texture, Vector2 position, float rotation, float scale, Color tint);  
-    DrawRectangle(pac_pos.x, pac_pos.y, pac_man_sprite.width*sprite_scale+3, 
-                    pac_man_sprite.height*sprite_scale+3, GREEN);
+    DrawRectangle(pac_pos.x, pac_pos.y, pm_width, pm_height, GREEN);
+    Rectangle pm_hitbox {pac_pos.x, pac_pos.y, pm_width, pm_height};
 
     DrawTextureEx(pac_man_sprite, pac_pos, 0, sprite_scale, WHITE);    
 }
 
 void PacMan::load_sprites(){
     pac_man_sprite = LoadTexture("../src/games/src/pac_man/assets/pactest.png");
-
+    pm_width = pac_man_sprite.width*sprite_scale+3;
+    pm_height = pac_man_sprite.height*sprite_scale+3;
 }
 
 
 void PacMan::get_input(){
     if((IsKeyDown(KEY_RIGHT))){
-        pac_direction = {1,0};
+        pending_direction = {1,0};
     }
     if((IsKeyDown(KEY_LEFT))){
-        pac_direction = {-1,0};
+        pending_direction = {-1,0};
     }
     if((IsKeyDown(KEY_UP))){
-        pac_direction = {0,-1};
+        pending_direction = {0,-1};
     }
     if((IsKeyDown(KEY_DOWN))){
-        pac_direction = {0,1};
+        pending_direction = {0,1};
     }
 }
 
 
 void PacMan::update_pac(){
+    // TODO: FIND MORE EFFICIENT WAY TO DETECT COLLISION
     get_input();
-    pac_pos = Vector2Add(pac_pos, pac_direction); //move pacman
+    
+    // Check if the pending direction can be applied.
+    Vector2 potentialPos = Vector2Add(pac_pos, pending_direction);
+    Rectangle potentialHitbox = { potentialPos.x, potentialPos.y, pm_width, pm_height };
+    bool turnPossible = true;
+
+    // Iterate over the map grid for potential turn collision.
+    for (int i = 0; i < map.numrow_get(); i++){
+        for (int j = 0; j < map.numcol_get(); j++){
+            if(map.get_cell(i, j) == 1){
+                Rectangle wallRect = {
+                    j * map.cellsize_get() + map.mid_mapx_get(), 
+                    i * map.cellsize_get() + 5,
+                    map.cellsize_get(),
+                    map.cellsize_get()
+                };
+
+                if(CheckCollisionRecs(potentialHitbox, wallRect)){
+                    turnPossible = false;
+                    break;
+                }
+            }
+        }
+        if(!turnPossible) break;
+    }
+    
+    // If the turn is available, update the current direction.
+    if(turnPossible){
+        pac_direction = pending_direction;
+    }
+    
+    // Calculate the new position based on the current pac_direction.
+    Vector2 newPos = Vector2Add(pac_pos, pac_direction);
+    Rectangle newHitbox = { newPos.x, newPos.y, pm_width, pm_height };
+    bool collision = false;
+
+    // Check collision for new position.
+    for (int i = 0; i < map.numrow_get(); i++){
+        for (int j = 0; j < map.numcol_get(); j++){
+            if(map.get_cell(i, j) == 1){
+                Rectangle wallRect = {
+                    j * map.cellsize_get() + map.mid_mapx_get(), 
+                    i * map.cellsize_get() + 5,
+                    map.cellsize_get(),
+                    map.cellsize_get()
+                };
+
+                if(CheckCollisionRecs(newHitbox, wallRect)){
+                    collision = true;
+                    break;
+                }
+            }
+        }
+        if(collision) break;
+    }
+
+    // Update pac position only if there's no collision.
+    if(!collision){
+        pac_pos = newPos;
+    }
 }
 
 void PacMan::run_game() {
 
     InitWindow(sw, sh, "NAM-CAP");
-    SetTargetFPS(60);
+    SetTargetFPS(80);
     //ToggleFullscreen();
 
     map.initiate_map();
